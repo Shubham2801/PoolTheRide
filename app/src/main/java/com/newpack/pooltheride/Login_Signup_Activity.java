b1ed2b6d17e4e23c65a2bf9250bd7e0a306e3b64
lakshay1701 <lakshaygupta1701@gmail.com>
2017-01-09 19:03:53 +0530

    login screen ui

---
+++ app/src/main/java/com/newpack/pooltheride/Login_Signup_Activity.java
@@ -0,0 +1,13 @@
+package com.newpack.pooltheride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Signup_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__signup_);
+    }
+}
